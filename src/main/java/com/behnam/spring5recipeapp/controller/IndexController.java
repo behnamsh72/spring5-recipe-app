fd4341e90a59d09b83a6545ca408fe3dd242ee96
Behnam <behnamsh72@gmail.com>
2020-03-27 05:31:31 +0430

    create recipes and notes entities

--- src/main/java/com/behnam/spring5recipeapp/controller/IndexController.java
+++ src/main/java/com/behnam/spring5recipeapp/controller/IndexController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    public IndexController() {
+        System.out.println("Controller constructor");
+    }
+
     @RequestMapping({"","/","index"})
     public String getIndexPage(){
-
+        System.out.println("Some message to say... 1234");
         return "index";
     }
 }
