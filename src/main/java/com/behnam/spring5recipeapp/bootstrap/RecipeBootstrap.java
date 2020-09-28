1473e22fd5663b113cc3902a446c60e1d65d0d8b
Behnam <behnamsh72@gmail.com>
2020-09-28 23:21:33 +0330

    implement recipe categories and ... on show.html file

--- src/main/java/com/behnam/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/behnam/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -122,6 +122,11 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
 
+        guacRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply recipe");
+
+
         recipes.add(guacRecipe);
 
         //Yummy Tacos
